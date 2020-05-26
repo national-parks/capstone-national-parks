3113853ebb23694181359ff245a36187ce0fe640
Sarah Beck <sarah.lizzie.beck@gmail.com>
2020-05-26 10:54:14 -0600

    Added shared components, added header base

--- app/src/index.js
+++ app/src/index.js
@@ -3,17 +3,21 @@ import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css';
 import {BrowserRouter} from "react-router-dom";
 import {Route, Switch} from "react-router";
+
 import {FourOhFour} from "./pages/FourOhFour";
 import {Home} from "./pages/Home";
+import {Header} from "./sharedComponents/Header"
 
 const Routing = () => (
 	<>
-	<BrowserRouter>
-	<Switch>
-	<Route exact path="/" component={Home}/>
-<Route component={FourOhFour}/>
-</Switch>
-</BrowserRouter>
-</>
+		<BrowserRouter>
+			<Header />
+			<Switch>
+				<Route exact path="/" component={Home}/>
+				<Route component={FourOhFour}/>
+			</Switch>
+		</BrowserRouter>
+	</>
 );
+
 ReactDOM.render(<Routing/>, document.querySelector('#root'));
