3e08e1b02f644c327a99225c972fe8a92dc7abac
John Romero <Jromero326@cnm.edu>
2020-06-03 14:24:35 -0600

    Added buttons and bootstrap carousel

--- app/src/pages/ParkPage.js
+++ app/src/pages/ParkPage.js
@@ -42,5 +42,25 @@ export const ParkPage = () => {
 	</Carousel.Item>
 </Carousel>
 
+		<section className="park-description">
+			<div className="description">
+				<div className="float-md-right">
+					<a className="btn btn-primary mb-5 pm-5" href="#" role="button">Favorite</a>
+					<button className="btn btn-primary mb-5 pm-5" role="button">Wish List</button>
+					<button className="btn btn-primary mb-5 pm-5" role="button">Visited</button>
+					<p className="park-info mb-5 pm-5">
+					igula semper id id. Libero in mi neque id et quibusdam, viverra turpis aenean at in integer, a egestas,
+						dictumst vel mauris eu in dictum vel, sem a at consequat. Aptent feugiat consectetuer
+						morbi tortor a voluptatem, ipsum est, varius fames ipsum augue sed sit. Vel posuere quis eros duis at, non
+						vulputate ac sollicitudin, vel tincidunt mauris felis vel imperdiet nisl. Tortor
+						nostra pellentesque neque suspendisse fringilla. Pede ut ut hendrerit mauris, natoque lobortis sociis tellus
+						commodo. Eget nam, tristique nunc sed vitae in proin sit. Mauris etiam suspendisse
+						massa enim aenean morbi, amet risus pede orci, quis enim pellentesque vivamus nec, augue hendrerit tempor
+						nec arcu urna. Posuere suspendisse sodales erat qui risus, suscipit nec vestibulum semper,
+						sit sed proin, ac lacinia sem massa id, tincidunt leo in nulla. Lacinia id, sodales purus nibh in elementum
+						nec metus, elit dignissim pellentesque sit vel phasellus similique.</p>
+				</div>
+			</div>
+		</section>
 </>
 )}
