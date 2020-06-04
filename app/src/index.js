92da4d048164286f4e29b5586e69d0c324a51334
Sarah Beck <sarah.lizzie.beck@gmail.com>
2020-06-03 21:53:57 -0600

    minor fix

--- app/src/index.js
+++ app/src/index.js
@@ -6,11 +6,11 @@ import {Route, Switch} from "react-router";
 import {FourOhFour} from "./pages/FourOhFour";
 import {Home} from "./pages/Home";
 import {ParkSearch} from './pages/ParkSearch/ParkSearch';
+import { CampgroundSearch } from './pages/CampgroundSearch/CampgroundSearch'
 
 import 'bootstrap/dist/css/bootstrap.css';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {faDumpster, faToiletPaper, faWifi, faShower, faMobileAlt, faTshirt, faPoop, faCashRegister, faCaravan, faTint, faIgloo, faFireAlt, faTheaterMasks, faBox} from '@fortawesome/free-solid-svg-icons'
-import { CampgroundSearch } from './pages/CampgroundSearch/CampgroundSearch'
 
 library.add(faDumpster, faToiletPaper, faWifi, faShower, faMobileAlt, faTshirt, faPoop, faCashRegister, faCaravan, faTint, faIgloo, faFireAlt, faTheaterMasks, faBox);
 
