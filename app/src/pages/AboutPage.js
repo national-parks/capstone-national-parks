09cdaf032937f3def7a6db19021bfd2af17b0f6d
Mauricio Madrid <maumadri@yahoo.com>
2020-06-08 12:33:10 -0600

    updated meet John section

--- app/src/pages/AboutPage.js
+++ app/src/pages/AboutPage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+image imageA from "./img/person.png"
 
 export const AboutPage = () => {
   return (
@@ -22,37 +23,42 @@ export const AboutPage = () => {
         </p>
       </div>
       <div className="container m-5">
-        <div className="card-group">
-          <div className="card pr-5">
-            <img src="../../saraBeck.png" className="card-img-top" alt="Sara Beck"></img>
+        <h5>Meet Sara</h5>
+        <div className="card mb-3" style="max-width: 540px;">
+          <div className="row no-gutters">
+            <div className="col-md-4">
+              <img src= {imageA} className="card-img" alt="Temp picture" />
+            </div>
+            <div className="col-md-8">
               <div className="card-body">
-                <h5 className="card-title">Meet Sara Beck</h5>
+                <h5 className="card-title">Card title</h5>
                 <p className="card-text">This is a wider card with supporting text below as a natural lead-in to
                   additional content. This content is a little bit longer.</p>
                 <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
               </div>
-          </div>
-          <div className="card pr-5">
-            <img src="../../johnRomero.png" className="card-img-top" alt="John Romero"></img>
-              <div className="card-body">
-                <h5 className="card-title">Meet John Romero</h5>
-                <p className="card-text">This card has supporting text below as a natural lead-in to additional
-                  content.</p>
-                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-              </div>
-          </div>
-          <div className="card">
-            <img src="../../pwpImg.jpeg" className="card-img-top" alt="Mauricio Madrid"></img>
-              <div className="card-body">
-                <h5 className="card-title">Meet Mauricio Madrid</h5>
-                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to
-                  additional content. This card has even longer content than the first to show that equal height
-                  action.</p>
-                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-              </div>
+            </div>
           </div>
         </div>
       </div>
+      <div className="container m-5">
+        <h5>Meet John</h5>
+        <div className="card mb-3" style="max-width: 540px;">
+          <div className="row no-gutters">
+            <div className="col-md-4">
+              <img src= {imageB} className="card-img" alt="Picture of John" />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to
+                  additional content. This content is a little bit longer.</p>
+                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </>
   )
 }
