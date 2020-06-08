7aae4bea2cc4687ac78d0f1e8733226282631d8f
Mauricio Madrid <maumadri@yahoo.com>
2020-06-08 12:35:42 -0600

    updated meet Mauricio section

--- app/src/pages/AboutPage.js
+++ app/src/pages/AboutPage.js
@@ -1,5 +1,7 @@
 import React from 'react'
 image imageA from "./img/person.png"
+image imageB from "./img/person.png"
+image imageC from "./img/person.png"
 
 export const AboutPage = () => {
   return (
@@ -58,7 +60,24 @@ export const AboutPage = () => {
           </div>
         </div>
       </div>
-
+      <div className="container m-5">
+        <h5>Meet Mauricio</h5>
+        <div className="card mb-3" style="max-width: 540px;">
+          <div className="row no-gutters">
+            <div className="col-md-4">
+              <img src= {imageC} className="card-img" alt="Picture of Mauricio" />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to
+                  additional content. This content is a little bit longer.</p>
+                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
