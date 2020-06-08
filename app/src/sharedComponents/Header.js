9ec2bd80a134dfb59cdcf4a78c94e9bc15c31df8
John Romero <Jromero326@cnm.edu>
2020-06-08 17:56:17 -0600

    corrected the header spacing issue. DO not imort bootstrap css on header pag, if already importing on index.js. clashes

--- app/src/sharedComponents/Header.js
+++ app/src/sharedComponents/Header.js
@@ -1,6 +1,5 @@
 import React from "react"
 import "./Header.css"
-import "bootstrap/dist/css/bootstrap.css"
 import { Navbar } from 'react-bootstrap'
 import { Nav } from 'react-bootstrap'
 import { NavDropdown } from 'react-bootstrap'
@@ -10,24 +9,26 @@ import { Button } from 'react-bootstrap'
 export const Header = () => {
 
 	return (<>
-			<Navbar expand="lg">
-				<Navbar.Brand href="#home">National Parks</Navbar.Brand>
-				<Navbar.Toggle aria-controls="basic-navbar-nav" />
-				<Navbar.Collapse id="basic-navbar-nav">
-					<Nav className="ml-auto">
-						<Nav.Link href="#home">Home</Nav.Link>
-						<NavDropdown title="Browse Parks" id="basic-nav-dropdown">
-							<NavDropdown.Item href="#action/3.1">By Ammenities</NavDropdown.Item>
-							<NavDropdown.Item href="#action/3.2">By Location</NavDropdown.Item>
-							<NavDropdown.Item href="#action/3.3">Search</NavDropdown.Item>
-					</NavDropdown>
-						<Nav.Link href="#home">About Us</Nav.Link>
-					</Nav>
-					<Button inline>
-						<Button variant="outline-dark">Login</Button>
-				</Button>
-				</Navbar.Collapse>
-			</Navbar>
+<header>
+	<Navbar expand="lg">
+		<Navbar.Brand href="#home">National Parks</Navbar.Brand>
+		<Navbar.Toggle aria-controls="basic-navbar-nav" />
+		<Navbar.Collapse id="basic-navbar-nav">
+			<Nav className="ml-auto">
+				<Nav.Link href="#home">Home</Nav.Link>
+				<NavDropdown title="Browse Parks" id="basic-nav-dropdown">
+					<NavDropdown.Item href="#action/3.1">By Ammenities</NavDropdown.Item>
+					<NavDropdown.Item href="#action/3.2">By Location</NavDropdown.Item>
+					<NavDropdown.Item href="#action/3.3">Search</NavDropdown.Item>
+				</NavDropdown>
+				<Nav.Link href="#home">About Us</Nav.Link>
+			</Nav>
+			<Button inline>
+				<Button variant="outline-dark">Login</Button>
+			</Button>
+		</Navbar.Collapse>
+	</Navbar>
+</header>
 		</>
 	)
 };
