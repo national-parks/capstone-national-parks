9ff1b4f74100cab7d345bcf02b83f5e573a8faea
Mauricio Madrid <maumadri@yahoo.com>
2020-06-08 12:47:59 -0600

    updated ProfilePage.js profile section.

--- app/src/pages/ProfilePage.js
+++ app/src/pages/ProfilePage.js
@@ -4,13 +4,36 @@ export const ProfilePage = () => {
   return (
     <>
       <div className="container m-5">
-        <h1>Profile</h1>
-        <p>Welcome to the National Parks website created by student interns at CNM Ingenuity, INC.
-          On these pages you will find information regarding the National Parks of the United States of America.
-          Each park page will include a list of valuable, important, and timely information for anyone
-          interested in visiting or just learning about what the National Parks have to offer to enjoy the great
-          outdoors.</p>
+        <form>
+          <div id="profilePic">
+            <img src="img/person.png" className="img-fluid img-thumbnail" alt="Picture Place Holder">
+          </div>
+          <div className="form-group row">
+            <label htmlFor="colFormLabel1" className="col-sm-2 col-form-label">Name </label>
+            <div className="col-sm-10">
+              <input type="email" className="form-control" id="colFormLabel1" placeholder="col-form-label">
+            </div>
+          </div>
+        </form>
+        <form>
+          <div className="form-group row">
+            <label htmlFor="colFormLabel2" className="col-sm-2 col-form-label">Email </label>
+            <div className="col-sm-10">
+              <input type="email" className="form-control" id="colFormLabel2" placeholder="col-form-label">
+            </div>
+          </div>
+        </form>
+        <form>
+          <div className="form-group row">
+            <label htmlFor="colFormLabel3" className="col-sm-2 col-form-label">Phone Number </label>
+            <div className="col-sm-10">
+              <input type="email" className="form-control" id="colFormLabel3" placeholder="col-form-label">
+            </div>
+          </div>
+          <button type="button" className="btn btn-primary">Edit Profile</button>
+        </form>
       </div>
+
     </>
   )
 }
