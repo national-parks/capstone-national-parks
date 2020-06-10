f4c341ff4e5a63540b501f66899561848e972cb9
Sarah Beck <sarah.lizzie.beck@gmail.com>
2020-06-10 16:57:38 -0600

    added selectProfileByProfileId mysql function, controller, route

--- apis/src/controllers/profile.controller.ts
+++ apis/src/controllers/profile.controller.ts
@@ -2,12 +2,25 @@ import {NextFunction, Request, Response} from "express";
 import {Status} from "../../utils/interfaces/status";
 import {Profile} from "../../utils/interfaces/profile";
 import {selectProfileByProfileActivationToken} from "../../utils/profile/selectProfileByProfileActivationToken";
+import {selectProfileByProfileId} from "../../utils/profile/selectProfileByProfileId";
 
 
 export async function getProfileByProfileActivationToken(request: Request, response: Response, nextFunction: NextFunction) {
     try {
         const {activationToken} = request.params
         const data = await selectProfileByProfileActivationToken(activationToken)
+        console.log(data)
+        const status: Status = {status: 200, data, message: null}
+        return response.json(status)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function getProfileByProfileId(request: Request, response: Response, nextFunction: NextFunction) {
+    try {
+        const {ProfileId} = request.params
+        const data = await selectProfileByProfileId(ProfileId)
         console.log(data)
         const status: Status = {status: 200, data, message: null}
         return response.json(status)
--- apis/src/routes/profile.route.ts
+++ apis/src/routes/profile.route.ts
@@ -1,8 +1,12 @@
 import {Router} from "express"
 import {indexController} from '../controllers/index.controller'
 import {getProfileByProfileActivationToken} from "../controllers/profile.controller";
+import {getProfileByProfileId} from "../controllers/profile.controller";
 
 export const profileRoute = Router()
+
+profileRoute.route('/:profileId')
+    .get(getProfileByProfileId)
 
 profileRoute.route('/activate/:activationToken')
     .get(getProfileByProfileActivationToken)
