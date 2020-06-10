5156e23801ea5f8f390f807b621360af1b3744c2
Sarah Beck <sarah.lizzie.beck@gmail.com>
2020-06-10 14:11:28 -0600

    minor mysql change to tables to include pictures

--- sql/national-parks.sql
+++ sql/national-parks.sql
@@ -16,7 +16,7 @@ DROP TABLE IF EXISTS profile;
 CREATE TABLE profile (
     profileId BINARY(16) NOT NULL,
     profileActivationToken CHAR(32),
-    profileAvatar,
+    profileAvatar BLOB,
     profileDisplayName VARCHAR(16) NOT NULL,
     profileEmail VARCHAR(128) NOT NULL,
     profileHash VARCHAR(32) NOT NULL,
