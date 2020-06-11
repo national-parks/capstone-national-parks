816c58e3fb1f9424ac904efba25bc80e918cc214
Sarah Beck <sarah.lizzie.beck@gmail.com>
2020-06-10 18:26:33 -0600

    added selectProfileBy email mysql function, controller, route

--- apis/src/routes/profile.route.ts
+++ apis/src/routes/profile.route.ts
@@ -1,12 +1,14 @@
 import {Router} from "express"
 import {indexController} from '../controllers/index.controller'
-import {getProfileByProfileActivationToken} from "../controllers/profile.controller";
-import {getProfileByProfileId} from "../controllers/profile.controller";
+import {getProfileByProfileId, getProfileByProfileEmail, getProfileByProfileActivationToken} from "../controllers/profile.controller";
 
 export const profileRoute = Router()
 
 profileRoute.route('/:profileId')
     .get(getProfileByProfileId)
+
+profileRoute.route('/:profileEmail')
+    .get(getProfileByProfileEmail)
 
 profileRoute.route('/activate/:activationToken')
     .get(getProfileByProfileActivationToken)
