78436a25aeb0bcc36c3c97c54fc3c943516d58fa
Mauricio Madrid <maumadri@yahoo.com>
2020-06-14 17:57:59 -0600

    added postParkImageId in parkImage.controller.ts file

--- apis/src/controllers/parkImage.controller.ts
+++ apis/src/controllers/parkImage.controller.ts
@@ -3,6 +3,8 @@ import {Status} from "../../utils/interfaces/status";
 import {ParkImage} from "../../utils/interfaces/parkImage";
 import {selectParkImageByParkImageParkId} from "../../utils/parkImage/selectParkImageByParkImageParkId";
 import {selectParkImageByParkImageId} from "../../utils/parkImage/selectParkImageByParkImageId";
+import {insertParkImage} from "../../utils/parkImage/insertParkImage";
+import {ParkImageRoute} from "../routes/parkImage.route";
 
 
 export async function getParkImagesByParkImageParkId(request: Request, response: Response, nextFunction: NextFunction) {
@@ -21,6 +23,18 @@ export async function getParkImagesByParkImageId(request: Request, response: Res
         const {parkImageId} = request.params
         const result = await selectParkImageByParkImageId(parkImageId)
         return response.json({status: 200, data: result, message: null})
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+export async function postParkImageId(request: Request, response: Response, nextFunction: NextFunction) {
+    try {
+        const {parkImageAttribution, parkImageContent, parkImageSubmitter} = request.body
+        const misquote: ParkImage = {parkImageId: null, parkImageParkId, parkImageUrl, parkImageCaption}
+        const result = await insertParkImage(postParkImageId)
+        return response.json({status: 200, data: null, message: result})
     } catch (error) {
         console.log(error)
     }
