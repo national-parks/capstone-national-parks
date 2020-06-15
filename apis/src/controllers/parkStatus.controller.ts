3c2b1914a993ee8877a2a3c09aeb18f0bea87c85
Sarah Beck <sarah.lizzie.beck@gmail.com>
2020-06-14 19:03:39 -0600

    added putparkstatus with controller/routes

--- apis/src/controllers/parkStatus.controller.ts
+++ apis/src/controllers/parkStatus.controller.ts
@@ -1,10 +1,22 @@
 import {NextFunction, Request, Response} from "express";
 import {Status} from "../../utils/interfaces/status";
 import {insertParkStatus} from "../../utils/parkStatus/insertParkStatus";
+import {updateParkStatus} from "../../utils/parkStatus/updateParkStatus";
 
 export async function postParkStatus(request: Request, response: Response, nextFunction: NextFunction) {
     try {
         const data = await insertParkStatus()
+        console.log(data)
+        const status: Status = {status: 200, data, message: null}
+        return response.json(status)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function putParkStatus(request: Request, response: Response, nextFunction: NextFunction) {
+    try {
+        const data = await updateParkStatus()
         console.log(data)
         const status: Status = {status: 200, data, message: null}
         return response.json(status)
--- apis/src/routes/parkStatus.route.ts
+++ apis/src/routes/parkStatus.route.ts
@@ -1,7 +1,8 @@
 import {Router} from "express";
-import {postParkStatus} from "../controllers/parkStatus.controller";
+import {postParkStatus, putParkStatus} from "../controllers/parkStatus.controller";
 
 export const parkStatusRoutes = Router()
 
 parkStatusRoutes.route("/")
     .post(postParkStatus)
+    .put(putParkStatus)
