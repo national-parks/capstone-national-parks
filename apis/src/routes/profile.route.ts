402e5f1ccfa23cd32d369ad64320b0d1d4a2f1ea
Sarah Beck <sarah.lizzie.beck@gmail.com>
2020-06-14 18:10:46 -0600

    added profile controllers/routers

--- apis/src/routes/profile.route.ts
+++ apis/src/routes/profile.route.ts
@@ -1,11 +1,17 @@
 import {Router} from "express"
 import {indexController} from '../controllers/index.controller'
-import {getProfileByProfileId, getProfileByProfileEmail, getProfileByProfileActivationToken} from "../controllers/profile.controller";
+import {
+    getProfileByProfileId,
+    getProfileByProfileEmail,
+    getProfileByProfileActivationToken,
+    postProfileController
+} from "../controllers/profile.controller";
 
 export const profileRoute = Router()
 
 profileRoute.route('/:profileId')
     .get(getProfileByProfileId)
+    .post(postProfileController)
 
 profileRoute.route('/:profileEmail')
     .get(getProfileByProfileEmail)
