993478540ae19d684a71f6c9e41fd1894da817ae
Sarah Beck <sarah.lizzie.beck@gmail.com>
2020-06-14 19:17:58 -0600

    fixing parkStatus put

--- apis/src/controllers/parkStatus.controller.ts
+++ apis/src/controllers/parkStatus.controller.ts
@@ -16,10 +16,11 @@ export async function postParkStatus(request: Request, response: Response, nextF
 
 export async function putParkStatus(request: Request, response: Response, nextFunction: NextFunction) {
     try {
-        const data = await updateParkStatus()
-        console.log(data)
-        const status: Status = {status: 200, data, message: null}
-        return response.json(status)
+        const {parkStatusStatus, parkStatusDate} = request.body
+        const {parkStatusProfileId, parkStatusParkId} = request.params;
+        const ParkStatus: ParkStatus = {parkStatusProfileId, parkStatusParkId, parkStatusStatus, parkStatusDate}
+        const result = await updateParkStatus(ParkStatus)
+        return response.json({status: 200, data: null, message: result})
     } catch (error) {
         console.log(error)
     }
