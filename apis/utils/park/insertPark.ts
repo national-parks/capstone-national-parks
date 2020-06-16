c6f1ddd0d0922e884d4ed3ead3703507c8c0ee57
John Romero <Jromero326@cnm.edu>
2020-06-16 15:23:19 -0600

    updated the insertPark.ts file.

--- apis/utils/park/insertPark.ts
+++ apis/utils/park/insertPark.ts
@@ -1,20 +1,16 @@
-import {Park} from "../interfaces/park";
+import {park} from "../interfaces/park";
 import {connect} from "../../src/database";
 
-/**
- * Inserts a Like into mySQL
- *
- * @param {Park} Park object to be inserted into mySQL
- **/
-export async function insertPark(Park: Park) {
+
+export async function insertPark(Park: park) {
     try {
 
         const mySqlConnection = await connect();
 
-        const mySqlQuery = "INSERT INTO `park`(parkId) VALUES(UUID_TO_BIN(:parkId), UUID_TO_BIN(:parkId))";
+        const mySqlQuery = "INSERT INTO park(parkId, parkContact, parkDescription, parkFullName, parkState, parkOperatingHours) VALUES(UUID_TO_BIN(UUID(:parkId)),:parkContact, :parkDescription, :parkFullName, :parkState, :parkOperatingHours)";
 
-        const [rows] = await mySqlConnection.execute(mySqlQuery, park)
-        return
+        const [rows] = await mySqlConnection.execute(mySqlQuery, Park)
+        return "park created successfully"
 
     } catch(error) {
         console.log(error)
