ce5d2809a4004fdfd8dec10f3676bbbccea7481c
John Romero <Jromero326@cnm.edu>
2020-06-22 08:32:11 -0600

    added my npi setupproxy and .gitignore

--- app/src/setupProxy.js
+++ app/src/setupProxy.js
@@ -2,7 +2,7 @@ const proxy = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(proxy('/apis', {
       logLevel: 'debug',
-      target: "http://138.197.210.89:8080/",
+      target: "http://165.227.21.218:8080/",
       changeOrigin: true,
     }
   ));
