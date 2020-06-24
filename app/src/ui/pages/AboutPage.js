93ab57d6716f7c182e8b386a55090d64856b185d
Mauricio Madrid <maumadri@yahoo.com>
2020-06-24 09:45:49 -0600

    updated AboutPage.js with John and Mauricio details

--- app/src/ui/pages/AboutPage.js
+++ app/src/ui/pages/AboutPage.js
@@ -47,8 +47,10 @@ export const AboutPage = () => {
             <div className="col-md-8">
               <div className="card-body">
                 <h5 className="card-title">Hello, I'm John Romero</h5>
-                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to
-                  additional content. This content is a little bit longer.</p>
+                <p className="card-text">A FullStack Web Developer.</p>
+                <p>Has a strong Customer Service background and wants to continue to learn and grow as a fullstack web developer.</p>
+                <p>Enjoys spending as much time as possible in the outdoors with family. Would like to learn about other Parks outside of New Mexico. (hunting, fishing and camping)
+                </p>
                 <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
               </div>
             </div>
@@ -65,8 +67,9 @@ export const AboutPage = () => {
             <div className="col-md-8">
               <div className="card-body">
                 <h5 className="card-title">Hello, I'm Mauricio Madrid</h5>
-                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to
-                  additional content. This content is a little bit longer.</p>
+                <p className="card-text">Born and raised in Las Vegas, New Mexico in the shadow of the Sangre de Cristo Mountains along the Pecos River where I learned to swim and fish.</p>
+                <p> I enjoy spending as much time as possible in the great outdoors. (Camping, fishing, hunting, mountain biking, snowboarding, golfing, photography, etc.)</p>
+
                 <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
               </div>
             </div>
