d6d63c02996aa13295216251607853231eda5ed6
John Romero <Jromero326@cnm.edu>
2020-06-24 18:11:31 -0600

    deleted the duplicates to the index.js.

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import {combineReducers} from 'redux'
 import parks from "./parks"
 import parkImages from "./parkImages"
 
 export default combineReducers({parks, parkImages})
-=======
-import {getAllParks} from "parks"
-import {getParkImagesByParkImageParkId} from "./parkImages"
 
-import { combineReducers } from 'redux'
 
-export default combineReducers({getAllParks, getParkImagesByParkImageParkId})
->>>>>>> john-proxy
